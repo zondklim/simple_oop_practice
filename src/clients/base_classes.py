cf6f4fb7ebd219d974f3357605593690aed10b43
zondklim <goger1263@gmail.com>
2026-08-17 19:54:20 +0200

    added some base classes

---
+++ src/clients/base_classes.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+
+from customer import Customer
+
+import hashlib
+from datetime import datetime 
+import random
+
+
+class AccountStatus(Enum):
+    ACTIVE = auto()
+    FROZEN = auto()
+    CLOSED = auto()
+
+
+class AbstractAccount(ABC):
+    def __init__(
+        self, 
+        owner_info: Customer,
+    ):
+        
+        self.owner_info = owner_info
+        
+        self._generate_card_number()
+        self.account_status: AccountStatus = AccountStatus.ACTIVE
+        self._balance: float = 0.
+        
+    
+    @abstractmethod
+    def deposit(self, amount: float):
+        pass
+    
+    @abstractmethod
+    def withdraw(self, amount: float):
+        pass
+    
+    @abstractmethod
+    def get_account_info(self, ):
+        pass
+    
+    def _generate_card_number(self, ):
+        self.card_number =  ''.join([str(random.randrange(10)) if i % 5 != 0 else ' ' for i in range(1, 20)])
+    
+    def _generate_id(self, ) -> None:
+        """Creating id from customer params and time.now as solt"""
+        info_string = self.owner_info.concat_to_str()
+        curr_dt = datetime.now().strftime("%Y%m%d%H%M%")
+        
+        self.id = hashlib.md5((info_string + curr_dt).encode()).hexdigest()
